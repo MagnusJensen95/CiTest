c07fd76e9864c19553eb2058a1cac131783111b4
Magnus <magnuskollip@hotmail.dk>
2018-04-26 14:47:40 +0200

    Auto migrate

--- GitGudCI/Program.cs
+++ GitGudCI/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using GitGudCI.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace GitGudCI
@@ -15,6 +18,20 @@ namespace GitGudCI
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+
+            var host = BuildWebHost(args);
+            
+                        // Seeds the database
+                       using (var scope = host.Services.CreateScope())
+                            {
+                var services = scope.ServiceProvider;
+                
+                                // Update migrations
+                var dbContext = services.GetRequiredService<ApplicationDbContext>();
+                dbContext.Database.Migrate();
+                           }
+            
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
